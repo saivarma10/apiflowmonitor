2924040ace87fa8a5a63eeddb1490862548eaf3f
satyanarayanavarma.kalidindi <satyanarayanavarma.kalidindi@opsramp.com>
2024-12-23 09:33:03 +0530

    db inital commit

--- internal/db/mysql.go
+++ internal/db/mysql.go
@@ -1,1 +1,34 @@
 package db
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+var db *sql.DB
+
+func init() {
+	db, err := sql.Open("mysql", "root:passwd@tcp(0.0.0.0:3306)/user")
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Ping()
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Print("Pong\n")
+	defer db.Close()
+}
+func createDatabase() {
+	_, err := db.Exec("CREATE DATABASE IF NOT EXISTS api_monitor")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Print("Successfully Created\n")
+	defer db.Close()
+
+}
