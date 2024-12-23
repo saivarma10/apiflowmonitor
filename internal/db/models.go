ed79b8b4d3ea46b7c261cae11e33001458a01032
satyanarayanavarma.kalidindi <satyanarayanavarma.kalidindi@opsramp.com>
2024-12-23 16:57:49 +0530

    db init

--- internal/db/models.go
+++ internal/db/models.go
@@ -1,1 +1,10 @@
 package db
+
+import "database/sql"
+
+func (d *DBConnection) Close() error {
+	return d.DB.Close()
+}
+func (d *DBConnection) GetDB() *sql.DB {
+	return d.DB
+}
--- internal/db/mysql.go
+++ internal/db/mysql.go
@@ -5,30 +5,22 @@ import (
 	"fmt"
 )
 
-var db *sql.DB
-
-func init() {
-	db, err := sql.Open("mysql", "root:passwd@tcp(0.0.0.0:3306)/user")
-
-	if err != nil {
-		panic(err)
-	}
-
-	err = db.Ping()
-
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Print("Pong\n")
-	defer db.Close()
+// var db *sql.DB
+type DBConnection struct {
+	DB *sql.DB
 }
-func createDatabase() {
-	_, err := db.Exec("CREATE DATABASE IF NOT EXISTS api_monitor")
-	if err != nil {
-		panic(err)
-	}
-	fmt.Print("Successfully Created\n")
-	defer db.Close()
 
+func NewConnection(host, user, password, dbname string, port int) (*DBConnection, error) {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", user, password, host, port, dbname)
+
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, fmt.Errorf("error connecting to database: %v", err)
+	}
+	err = db.Ping()
+	if err != nil {
+		return nil, fmt.Errorf("error pinging database: %v", err)
+	}
+
+	return &DBConnection{DB: db}, nil
 }
